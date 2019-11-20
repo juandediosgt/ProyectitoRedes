5a541d42b0d914b2229fe8d56960ac6941e6ab51
juandediosgt <50521468+juandediosgt@users.noreply.github.com>
2019-11-20 15:29:34 -0600

    Proyecto_CambioPassword_Producto_Usuarios

---
+++ src/java/beans/MenuPrincipalBean.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Juan de Dios
+ */
+public class MenuPrincipalBean implements Serializable {
+
+    /**
+     * Creates a new instance of MenuPrincipalBean
+     */
+    public MenuPrincipalBean() {
+    }
+    
+   public String irProducto(){
+       String resultado="producto";
+               return resultado;
+   }
+    public String nuevoUsuario(){
+        String resultado="nuevoUsuario";
+        return resultado;
+    }
+     public String cerrarSesion(){
+        String resultado="Cerrar";
+        return resultado;
+    }
+}
---
+++ src/java/pojos/Producto.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pojos;
+
+/**
+ *
+ * @author Juan de Dios
+ */
+public class Producto {
+    private String nombre;
+    private String descripcion;
+    private float precio_compra;
+    private float precio_venta;
+    private int cantidad_existencia;
+    private String idproveedor;
+    private String proveedor;
+    private int idcategoria;
+    private String categoria;
+    private int idestado;
+    private String estado;
+    /**
+     * Creates a new instance of Producto
+     */
+    public Producto(  ) {
+        
+    }
+    public Producto(String nombre, String descripcion, float precio_compra, float precio_venta, int cantidad_existencia, String idproveedor, String proveedor, int idcategoria,String categoria, int idestado, String estado){
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio_compra = precio_compra;
+        this.precio_venta = precio_venta;
+        this.cantidad_existencia = cantidad_existencia;
+        this.idproveedor = idproveedor;
+        this.proveedor = proveedor;
+        this.idcategoria = idcategoria;
+        this.categoria = categoria;
+        this.idestado = idestado;
+        this.estado= estado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float getPrecio_compra() {
+        return precio_compra;
+    }
+
+    public void setPrecio_compra(float precio_compra) {
+        this.precio_compra = precio_compra;
+    }
+
+    public float getPrecio_venta() {
+        return precio_venta;
+    }
+
+    public void setPrecio_venta(float precio_venta) {
+        this.precio_venta = precio_venta;
+    }
+
+    public int getCantidad_existencia() {
+        return cantidad_existencia;
+    }
+
+    public void setCantidad_existencia(int cantidad_existencia) {
+        this.cantidad_existencia = cantidad_existencia;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public int getIdcategoria() {
+        return idcategoria;
+    }
+
+    public void setIdcategoria(int idcategoria) {
+        this.idcategoria = idcategoria;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+
+    public int getIdestado() {
+        return idestado;
+    }
+
+    public void setIdestado(int idestado) {
+        this.idestado = idestado;
+    }
+
+    public String getIdproveedor() {
+        return idproveedor;
+    }
+
+    public void setIdproveedor(String idproveedor) {
+        this.idproveedor = idproveedor;
+    }
+    
+}
